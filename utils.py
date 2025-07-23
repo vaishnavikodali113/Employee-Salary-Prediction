ce73e83b8c521837f2e7dcef98d15b7541d7de6f
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-07-23 05:43:32 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,5 +0,0 @@
-def clean_strings(df):
-    obj_cols = df.select_dtypes(include="object").columns
-    for col in obj_cols:
-        df[col] = df[col].astype(str).str.strip().str.lower()
-    return df
