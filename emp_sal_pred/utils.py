79697f7d7dd5b3b5f5eff47c502b398ebe3e6c8e
vaishnavikodali113 <srivaishnavi1705@gmail.com>
2025-07-23 05:40:59 +0530

    Add files via upload

---
+++ emp_sal_pred/utils.py
@@ -0,0 +1,5 @@
+def clean_strings(df):
+    obj_cols = df.select_dtypes(include="object").columns
+    for col in obj_cols:
+        df[col] = df[col].astype(str).str.strip().str.lower()
+    return df
